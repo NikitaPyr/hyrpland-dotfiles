a5d3cc219e7a83aa74b50bcfeb11053a5ebc3e29
NikitaPyr <nikita.pyroschkin@gmail.com>
2025-11-16 14:05:51 +0300

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,65 +2,57 @@
 --           БАЗОВАЯ НАСТРОЙКА
 -- =========================================
 
--- Подсветка синтаксиса
-vim.cmd('syntax on') -- делает код цветным: ключевые слова, строки, комментарии
-
--- Отключаем совместимость с Vi
-vim.o.compatible = false -- включает все современные функции Vim/Neovim
-
--- Используем системный буфер обмена
-vim.o.clipboard = "unnamedplus" -- позволяет копировать/вставлять между Neovim и другими приложениями
-
--- =========================================
---           НОМЕРА СТРОК
--- =========================================
-
-vim.wo.number = true         -- показываем реальные номера строк
-vim.wo.relativenumber = true -- показываем относительные номера для удобных движений
+vim.o.compatible = false           -- отключаем совместимость с Vi
+vim.o.syntax = "on"                -- подсветка синтаксиса
+vim.o.clipboard = "unnamedplus"    -- использовать системный буфер обмена
+vim.o.termguicolors = true         -- поддержка 24-битных цветов
+vim.wo.cursorline = true           -- подсветка строки с курсором
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.signcolumn = "yes"           -- колонка для знаков всегда
 
 -- =========================================
 --           ОТСТУПЫ И ТАБЫ
 -- =========================================
 
-vim.o.tabstop = 4        -- ширина таба в 4 пробела
-vim.o.shiftwidth = 4     -- автоотступы на 4 пробела
-vim.o.expandtab = true   -- заменяем табы пробелами
-vim.o.smartindent = true -- умные отступы по структуре кода
-
--- =========================================
---           ВИЗУАЛЬНЫЕ ФИЧИ
--- =========================================
-
-vim.o.termguicolors = true -- поддержка 24-битных цветов
-vim.wo.cursorline = true   -- подсветка строки с курсором
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
 
 -- =========================================
 --           ПОИСК
 -- =========================================
 
-vim.o.ignorecase = true   -- поиск без учёта регистра
-vim.o.smartcase = true    -- если есть заглавные буквы, учитывается регистр
-vim.o.incsearch = true    -- интерактивный поиск по мере ввода
-vim.o.hlsearch = true     -- подсвечивать все совпадения после Enter
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.hlsearch = true
+vim.o.inccommand = "nosplit"       -- live preview для :%s
 
 -- =========================================
---           УДОБСТВО И БЛОКИРОВКА СТРЕЛОК
+--           SPLIT И ОКНА
 -- =========================================
 
-vim.o.mouse = ''          -- полностью отключаем мышь
-vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', { noremap = true, silent = true })
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- =========================================
---           ПОВЕДЕНИЕ SPLIT
+--           ОТКЛЮЧЕНИЕ СТРЕЛОК И МЫШИ
 -- =========================================
 
-vim.o.splitright = true   -- новые вертикальные окна открываются справа
-vim.o.splitbelow = true   -- новые горизонтальные окна открываются снизу
+vim.o.mouse = "" -- полностью отключаем мышь
+
+local opts = { noremap = true, silent = true }
+for _, mode in ipairs({"n", "i"}) do
+  for _, key in ipairs({"<Up>", "<Down>", "<Left>", "<Right>"}) do
+    vim.api.nvim_set_keymap(mode, key, "<Nop>", opts)
+  end
+end
+
+-- =========================================
+--           ПОДДЕРЖКА FILETYPE
+-- =========================================
+
+vim.cmd("filetype plugin indent on") -- включаем автоотступы под файлы
 
